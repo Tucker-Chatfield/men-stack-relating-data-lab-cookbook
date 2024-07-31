426c7c5b547669f4e830e86d4e8278f5ff294c2f
tucker-chatfield <tucker.chatfield@gmail.com>
2024-07-31 17:16:02 -0500

    Controller files setup, middleware setup

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const recipesController = require('./controllers/recipes.js');
+const ingredientsController = require('./controllers/ingredients.js');
+
+app.use('/auth', authController);
+app.use('/recipes', recipesController);
+app.use('/ingredients', ingredientsController);
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
